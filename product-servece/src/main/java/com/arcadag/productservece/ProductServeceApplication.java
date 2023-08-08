cbd4c9b780f934f609e7fc597445beb6c1821796
arcadag <piknik0711@yandex.ru>
2023-08-08 17:26:07 +0300

    init product-service module

---
+++ product-servece/src/main/java/com/arcadag/productservece/ProductServeceApplication.java
@@ -0,0 +1,13 @@
+package com.arcadag.productservece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServeceApplication.class, args);
+	}
+
+}
