0ec78dcf4a83959fecbc78509240a121421a993b
arcadag <piknik0711@yandex.ru>
2023-08-08 17:33:06 +0300

    init review-service module

---
+++ review-service/src/test/java/com/arcadag/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadag.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
