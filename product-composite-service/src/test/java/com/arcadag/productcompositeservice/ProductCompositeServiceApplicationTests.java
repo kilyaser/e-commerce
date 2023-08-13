74a67b735ee04690b699cac00e4a1ecbf4866eec
arcadag <piknik0711@yandex.ru>
2023-08-13 18:42:04 +0300

    added Dockerfile and docker profile

--- product-composite-service/src/test/java/com/arcadag/productcompositeservice/ProductCompositeServiceApplicationTests.java
+++ product-composite-service/src/test/java/com/arcadag/productcompositeservice/ProductCompositeServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.arcadag.productcompositeservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProductCompositeServiceApplicationTests {
 
 	@Test
