f1b41ac073772818681e0adb55560e04d4f0c6bd
Ilia Kalendarev <piknik0711@yandex.ru>
2023-11-01 18:00:03 +0300

    set health check

--- gateway/src/main/java/com/arcadag/gateway/HealthCheckConfiguration.java
+++ gateway/src/main/java/com/arcadag/gateway/HealthCheckConfiguration.java
@@ -1,8 +1,20 @@
 package com.arcadag.gateway;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.logging.Level.FINE;
 
 @Configuration
 @Slf4j
@@ -12,5 +24,26 @@ public class HealthCheckConfiguration {
 
     public HealthCheckConfiguration(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
+    }
+
+    @Bean
+    ReactiveHealthContributor healthCheckMicroservice() {
+        final Map<String, ReactiveHealthIndicator> registry = new LinkedHashMap<>();
+
+        registry.put("product",           () -> getHealth("http://product"));
+        registry.put("recommendation",    () -> getHealth("http://recommendation"));
+        registry.put("review",            () -> getHealth("http://review"));
+        registry.put("product-composite", () -> getHealth("http://product-composite"));
+
+        return CompositeReactiveHealthContributor.fromMap(registry);
+    }
+
+    private Mono<Health> getHealth(String baseUrl) {
+        String url = baseUrl + "/actuator/health";
+        log.debug("Setting up a call to the Health API on URL: {}", url);
+        return webClient.get().uri(url).retrieve().bodyToMono(String.class)
+                .map(s -> new Health.Builder().up().build())
+                .onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
+                .log(log.getName(), FINE);
     }
 }
